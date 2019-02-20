b38606faf4f24f66fa75e5276b1287165ea8cb8b
Alex Tran <alextran1357@gmail.com>
2019-02-19 21:49:23 -0800

    Deleted white spaces, lowercased letters. Also added a list of english words

--- substitution_ciphers_lab_1/substitution_ciphers_lab_1.py
+++ substitution_ciphers_lab_1/substitution_ciphers_lab_1.py
@@ -1,1 +1,15 @@
+#Function to decrypt a string
+def word_decrypt(user_string):
+    #first check for cesear decryption
+    MAX_ROT = 25 #25 because 26 would just loop in another check
 
+    #take out the white spaces of the string
+    user_string = user_string.lower().replace(" ","")
+    #print(user_string)
+    
+
+#Prompt the user to enter in the string they want to use
+print("Enter in the string to decrypt")
+user_string = input()
+
+word_decrypt(user_string)
