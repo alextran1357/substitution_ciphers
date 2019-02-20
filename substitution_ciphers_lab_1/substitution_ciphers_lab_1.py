d89b5bafdd22d5995f13d66047d3aa40582ee529
Alex Tran <alextran1357@gmail.com>
2019-02-20 01:16:18 -0800

    Added better comments

--- substitution_ciphers_lab_1/substitution_ciphers_lab_1.py
+++ substitution_ciphers_lab_1/substitution_ciphers_lab_1.py
@@ -1,4 +1,8 @@
-#Function to decrypt a string
+'''
+Function to decrypt a string
+
+@param {string} user_string - string that user enters to be decrypted
+'''
 def word_decrypt(user_string):
     #first check for cesear decryption
     MAX_ROT = 26
@@ -13,13 +17,34 @@ def word_decrypt(user_string):
         string_list = list(user_string)
         # Use ASCII code to rotate the letters 97-122
         for i in range (len(string_list)):
+            # Reset the ASCII after the letter 'z'
             if (ord(string_list[i]) + rotation_num > 122):
                 string_list[i] = (chr(ord(string_list[i]) + rotation_num - 26));
             else:
                 string_list[i] = (chr(ord(string_list[i]) + rotation_num));
         string_list = "".join(string_list)
         print(string_list)
+
+'''
+Input a string to check if it is a real sentence
+
+@param {string} string - string that is going to be checked
+@param {int} string_length_check - the amount of chars from the beginning that will be checked
+@param {int} char_check_length - the number of characters checked to see if it is an english word; either 4 or 5
+'''
+def check_string(string, string_length_check, char_check_length):
+    #Most common length for an english word is 4-5 characters
     
+
+'''
+Input a word to check if it exist in the list of words
+
+@param {string} word - word that is going to be check against a list of english words
+'''
+def check_word(word):
+    print("Hello")
+
+
 # Strings to use
 '''
 1. fqjcb rwjwj  vnjax  bnkhj  whxcq  nawjv  nfxdu  mbvnu  ujbbf  nnc
